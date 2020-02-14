f0ad027aa9bc8bd606f96b9fb1327fe10097bc25
Oleg Shapoval <Olegx4@ukr.net>
2020-02-14 18:23:13 +0200

    Created simple spring boot hello-world site

---
+++ src/main/java/olegx4/github/com/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package olegx4.github.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
